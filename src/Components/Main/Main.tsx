b60564c9af54b316ad5a96b37fb857593ed9a079
uros milenkovic <urosm4471@gmail.com>
2025-05-12 17:30:39 +0200

    fixed

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,11 +1,11 @@
-import logo from "../../assets/logo trasparente (1) (1).png";
+import logo from "../../assets/0. HOME LOGO 2.png";
 import "./Main.css";
 
 const Main: React.FC = () => {
   return (
-    <main className="row">
-      <div className="col-lg-12 d-flex justify-content-center pb-5">
-        <img src={logo} alt="homepage-logo-image" className=" img-homepage" />
+    <main className="row mt-5 pt-2">
+      <div className="col-lg-12 d-flex justify-content-center mt-5  pb-5">
+        <img src={logo} alt="homepage-logo-image" className="img-homepage" />
       </div>
     </main>
   );
